3da2bfbc41d03f88fe8b4b99ad4fcdf4e7b1da14
vaibhav71 <vaibhavpatil.siliconithub@gmail.com>
2022-12-20 19:40:36 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+// import logo from './logo.svg';
+// import './App.css';
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import SectionOne from "./components/SectionOne";
+import SectionTwo from "./components/SectionTwo";
+import SectionThree from "./components/SectionThree";
+import SectionFour from "./components/SectionFour";
+import SectionFive from "./components/SectionFive";
+
+function App() {
+  return (
+
+    <>
+
+      <Navbar />
+      <Header />
+      <SectionOne />
+      <SectionTwo />
+      <SectionThree />
+      <SectionFour />
+      <SectionFive />
+
+    </>
+  );
+}
+
+export default App;
